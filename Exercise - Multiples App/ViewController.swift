a8c3fd213e5619c4e26fa5801a018b94ce5ec962
Shaisan2 <shivam0sharma@icloud.com>
2017-05-12 18:55:32 -0400

    Made adjustments with UI and finished the app

--- Exercise - Multiples App/ViewController.swift
+++ Exercise - Multiples App/ViewController.swift
@@ -16,13 +16,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     
+    // Properties
+    var multiple = 0
+    var currentMultiple = 0
+    let maxMultipleAmount = 10
+    
     @IBAction func playButtonPressed(_ sender: UIButton!) {
-        
+        if (multipleTextField.hasText) {
+            logo.isHidden = true
+            multipleTextField.isHidden = true
+            playButton.isHidden = true
+            
+            multipleLabel.isHidden = false
+            addButton.isHidden = false
+            
+            multiple = Int(multipleTextField.text!)!
+            multipleTextField.resignFirstResponder()
+        }
     }
     
     @IBAction func addButtonPressed(_ sender: UIButton!) {
-        
+        if hasReachedMaxMultiples() {
+            restart()
+        } else {
+            multipleLabel.text = "\(currentMultiple) + \(multiple) = \(currentMultiple + multiple)"
+            currentMultiple += multiple
+        }
     }
     
+    func restart() {
+        logo.isHidden = false
+        multipleTextField.isHidden = false
+        playButton.isHidden = false
+            
+        multipleLabel.isHidden = true
+        addButton.isHidden = true
+            
+        multiple = 0
+        currentMultiple = 0
+        
+        multipleLabel.text = "Press Add to add!"
+    }
+    
+    func hasReachedMaxMultiples() -> Bool {
+        if ((currentMultiple/multiple)  == maxMultipleAmount) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
-
