1e71c19d6b8375f87ceb4f15dcf764e92128c969
Shaisan2 <shivam0sharma@icloud.com>
2017-05-12 18:28:19 -0400

    Connected the graphics with the ViewController

--- Exercise - Multiples App/ViewController.swift
+++ Exercise - Multiples App/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var multipleLabel: UILabel!
+    @IBOutlet weak var multipleTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBAction func playButtonPressed(_ sender: UIButton!) {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addButtonPressed(_ sender: UIButton!) {
+        
     }
-
-
+    
 }
 
